ad54c40c3860a7eeb4ae0f75f9a9f536eaba6c40
WoziKing <wzq23t@163.com>
2022-04-22 22:15:06 +0800

    first commit

--- admin/fun/out.php
+++
@@ -1,6 +0,0 @@
-<?php
-session_start();
-setcookie("admin_fix_3U5AvnlOFCpR6sTg", "", time()-3600*24);
-unset($_SESSION["admin_fix_3U5AvnlOFCpR6sTg"]);
-$_SESSION["admin_fix_3U5AvnlOFCpR6sTg"]=false;
-header("location:../index.php");
